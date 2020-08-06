33761c654eed2a1f91abfe2c600c9d00aa4bb841
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2020-08-06 14:47:54 +0800

    i

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "BOT-TOKEN-GOES-HERE"
-bot_user_name = "BOT-USER-NAME"
-URL = "THE-HEROKU-APP-LINK"
+bot_token = "1387009290:AAG_fBq3LcXQKdXL12fpJskceNfW8Nz-s5M"
+bot_user_name = "@SmatDoorLockBot"
+URL = "https://semartdoorlock.herokuapp.com/"
