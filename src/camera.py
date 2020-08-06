aab296c969d2997bc80fb4f0df976f42a2193c5a
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2020-08-06 15:08:43 +0800

    add files :zap:

--- src/camera.py
+++ src/camera.py
@@ -5,7 +5,7 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("files/rizkhal.jpg")
+obama_image = face_recognition.load_image_file("../files/rizkhal.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Create arrays of known face encodings and their names
